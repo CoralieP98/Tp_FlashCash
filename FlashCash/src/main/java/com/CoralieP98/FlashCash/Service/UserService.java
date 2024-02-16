1da7664acfa6bf7aa90469cac897962b26d0ee71
cocomeh <coralie.perez98@orange.fr>
2024-02-16 15:24:04 +0100

    Spring security started ThymeLeaf not done signIn and SignUp form html&Css done

--- FlashCash/src/main/java/com/CoralieP98/FlashCash/Service/UserService.java
+++ FlashCash/src/main/java/com/CoralieP98/FlashCash/Service/UserService.java
@@ -4,14 +4,16 @@ import com.CoralieP98.FlashCash.Model.Account;
 import com.CoralieP98.FlashCash.Model.User;
 import com.CoralieP98.FlashCash.Repository.UserRepository;
 import com.CoralieP98.FlashCash.Service.Form.SignUpForm;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
-
-    public UserService(UserRepository userRepository) {
+    private final PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registration(SignUpForm form){
